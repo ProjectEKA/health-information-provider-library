8ce2d513401b29ee514df0b0f359a6f9dd47910c
nexengineer <nexengineer@gmail.com>
2020-01-06 13:23:27 +0530

    Updating Library

--- health-information-provider-library/Patient/Models/Request/PatientLinkRequest.cs
+++ health-information-provider-library/Patient/Models/Request/PatientLinkRequest.cs
@@ -2,13 +2,11 @@ using System.Text.Json.Serialization;
 using System.Xml.Serialization;
 
 namespace HipLibrary.Patient.Models.Request
+
 {
     public class PatientLinkRequest
     {
-        [JsonPropertyName("token")]
-        [XmlElement("token")]
         public string Token { get; }
-
         public PatientLinkRequest(string token)
         {
             Token = token;
